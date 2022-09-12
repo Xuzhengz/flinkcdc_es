ed273af3804b51655e2379160f789168f4750201
Xuzhengz <2350122040@qq.com>
2022-09-12 17:43:37 +0800

    2022-09-12

---
+++ src/main/java/com/ocean/flinkcdc/PostgreToElasticSearch.java
@@ -0,0 +1,60 @@
+package com.ocean.flinkcdc;
+
+import com.ververica.cdc.connectors.postgres.PostgreSQLSource;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.runtime.checkpoint.Checkpoint;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+import java.sql.Savepoint;
+
+/**
+ * @author 徐正洲
+ * @date 2022/9/12-16:58
+ */
+public class PostgreToElasticSearch {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+//        开启 Checkpoint, 每隔 5 秒钟做一次 CK
+        env.enableCheckpointing(60000L);
+//        指定 CK 的一致性语义
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+//        设置任务关闭的时候保留最后一次 CK 数据
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+//        指定从 CK 自动重启策略
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 2000L));
+//        设置状态后端
+        env.setStateBackend(new FsStateBackend("hdfs://172.16.11.151:8020/flinkCDC"));
+//        设置访问 HDFS 的用户名
+        System.setProperty("HADOOP_USER_NAME", "root");
+
+
+//        自定义cdc数据源
+        SourceFunction<String> sourceFunction = PostgreSQLSource.<String>builder()
+                .hostname("172.16.8.222")
+                .port(5432)
+                .database("dzsj") // monitor postgres database
+                .schemaList("bzdz")  // monitor inventory schema
+                .tableList("bzdz.bzdz_all") // monitor products table
+                .username("postgres")
+                .password("1Qaz2wsx")
+                .slotName("flink_cdc_postgre")
+                .decodingPluginName("pgoutput")
+                .deserializer(new MyJsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
+                .build();
+
+
+
+        DataStreamSource<String> pgStream = env.addSource(sourceFunction);
+        pgStream.print();
+
+        env.execute();
+
+    }
+}
