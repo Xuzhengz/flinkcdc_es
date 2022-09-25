9a29940c48ee513084abed43998341a6940f5923
Xuzhengz <2350122040@qq.com>
2022-09-25 17:23:53 +0800

    2022-09-25-自定义数据源

---
+++ src/main/java/com/ocean/flinkcdc/View/PostgreToEs6Streaming.java
@@ -0,0 +1,84 @@
+package com.ocean.flinkcdc.View;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ocean.flinkcdc.constants.PropertiesContants;
+import com.ocean.flinkcdc.sink.ElasticsearchSink6;
+import com.ocean.flinkcdc.source.MyJsonDebeziumDeserializationSchema;
+import com.ocean.flinkcdc.utils.PKCS5PaddingUtils;
+import com.ververica.cdc.connectors.postgres.PostgreSQLSource;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+/**
+ * @author 徐正洲
+ * @date 2022/9/25-14:11
+ * <p>
+ * Flink写入es
+ */
+public class PostgreToEs6Streaming {
+    public static void main(String[] args) throws Exception{
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        /**
+         * 1、Flink_CDC_POSTGRESQL 实时加密数据源,source只允许并行度为“1”。
+         */
+        SourceFunction<JSONObject> sourceFunction = PostgreSQLSource.<JSONObject>builder()
+                .hostname(PropertiesContants.PGHOSTNAME)
+                .port(PropertiesContants.PGPORT)
+                .database(PropertiesContants.PGDATABASE)
+                .schemaList(PropertiesContants.PGSCHEMALIST)
+                .tableList(PropertiesContants.PGTABLELIST)
+                .username(PropertiesContants.PGUSERNAME)
+                .password(PropertiesContants.PGPASSWORD)
+                .slotName("flink_cdc_pg_es")
+                .decodingPluginName("pgoutput")
+                .deserializer(new MyJsonDebeziumDeserializationSchema())
+                .debeziumProperties(PropertiesContants.properties)
+                .build();
+
+        DataStreamSource<JSONObject> pgStream = env.addSource(sourceFunction).setParallelism(1);
+        /**
+         * 2、无状态计算，解密操作
+         */
+
+        SingleOutputStreamOperator<JSONObject> PaddingStream = pgStream.map(new MapFunction<JSONObject, JSONObject>() {
+            @Override
+            public JSONObject map(JSONObject jsonObject) throws Exception {
+                //
+                JSONObject data = (JSONObject) jsonObject.get("data");
+                //重新给JSON赋值解密数据和过滤字段
+                JSONObject dataJson = new JSONObject();
+                JSONObject geoJson = new JSONObject();
+                geoJson.put("lat", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("gd_wd")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                geoJson.put("lon", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("gd_jd")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("title", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("title")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("address", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("address")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("xzqh", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("xzqh")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("pcs", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("pcs")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("coordinate02", geoJson);
+                dataJson.put("source", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("source")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("location_id", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("location_id")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("jdname", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("jdname")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("jwname", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("jwname")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                dataJson.put("address_type", PKCS5PaddingUtils.decrypt(String.valueOf(data.get("address_type")), PKCS5PaddingUtils.EPIDEMIC_KEY));
+                jsonObject.put("filterJson",dataJson);
+                jsonObject.remove("data");
+                return jsonObject;
+            }
+        }).setParallelism(3);
+
+
+        /**
+         * 自定义ElasticSearch6 Sink写入。
+         */
+
+        PaddingStream.addSink(new ElasticsearchSink6()).setParallelism(3);
+
+
+        env.execute("Ocean_Postgre_To_ES6_Streaming");
+
+
+    }
+}
---
+++ src/main/java/com/ocean/flinkcdc/sink/ElasticsearchSink6.java
@@ -0,0 +1,97 @@
+package com.ocean.flinkcdc.sink;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ocean.flinkcdc.constants.PropertiesContants;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+
+/**
+ * @author 徐正洲
+ * @date 2022/9/25-15:17
+ * 升级版自定义Es写入操作。
+ */
+public class ElasticsearchSink6 extends RichSinkFunction<JSONObject> {
+    public static RestHighLevelClient esClient;
+    public static final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+
+    /**
+     * 定义加密的es客户端
+     */
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(PropertiesContants.ESUSERNAME,PropertiesContants.ESPASSWORD));  //es账号密码
+        esClient =new RestHighLevelClient(RestClient.builder(new HttpHost(PropertiesContants.ESHOSTNAME,PropertiesContants.ESPORT))
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        httpClientBuilder.disableAuthCaching();
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                })
+        );
+    }
+
+    /**
+     * 实时同步postgresql操作
+     */
+    @Override
+    public void invoke(JSONObject value, Context context) throws Exception {
+        String  operation = String.valueOf(value.get("operation"));
+        if ("create".equals(operation)||"update".equals(operation)){
+            BulkRequest request = new BulkRequest();
+            request.add(new IndexRequest().
+                    index(PropertiesContants.ESINDEX)
+                    .type(PropertiesContants.ESTYPE)
+                    .id(value.getJSONObject("filterJson")
+                            .getString("location_id"))
+                    .source(value.getJSONObject("filterJson"))
+            );
+            BulkResponse response = esClient.bulk(request, RequestOptions.DEFAULT);
+            if (response.hasFailures() == false) {
+                System.out.println("操作成功" + "\t花费时长：" + response.getTook() + "\t主键id：" + value.getJSONObject("filterJson").get("location_id"));
+            } else {
+                System.out.println(response.buildFailureMessage());
+            }
+        } else if ("delete".equals(operation)) {
+            DeleteRequest deleteRequest = new DeleteRequest(PropertiesContants.ESINDEX,
+                    PropertiesContants.ESTYPE,
+                    value.getJSONObject("filterJson").getString("location_id")
+            );
+            DeleteResponse deleteResult = esClient.delete(deleteRequest, RequestOptions.DEFAULT);
+            System.out.println("操作类型：" + deleteResult.getResult() +
+                    "  删除数据id为：" + value.getJSONObject("filterJson").getString("location_id"));
+        }
+
+    }
+
+    /**
+     * 关闭es客户端操作
+     */
+    @Override
+    public void close() throws Exception {
+        if (esClient!=null){
+            try {
+                esClient.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
